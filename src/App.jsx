1b7b300453d40f5a9f212f31acc70b2c61801891
Vinicius Moura <viniciusmdcm@gmail.com>
2025-06-17 19:10:44 -0300

    Create task component - layout

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,7 @@ function App() {
         <Todo todo={todo} />
       ))}
     </div>
-    {/* <TodoForm /> */}
+    {<TodoForm />}
   </div>
 }
 
--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
+import '../assets/styles/todoForm.scss';
 
 function TodoForm() {
   return <div className='todo-form'>
     <h2>Criar Tarefa</h2>
-    <form>
-        <input type="text" placeholder='Digite o título'/>
-        <select>
-            <option value="">Selecione uma categoria</option>
-            <option value="Trabalho">Trabalho</option>
-            <option value="Pessoal">Pessoal</option>
-            <option value="Estudos">Estudos</option>
+    <form className='form'>
+        <input type='text' className='form-element' placeholder='Digite o título'/>
+        <select className='form-element'>
+            <option value='' disabled selected hidden>Selecione uma categoria</option>
+            <option value='Trabalho'>Trabalho</option>
+            <option value='Pessoal'>Pessoal</option>
+            <option value='Estudos'>Estudos</option>
         </select>
         <button type='submit'>Adicionar tarefa</button>
     </form>
